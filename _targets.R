c6e8e87f5913d03fc5edc824d24dc26ca4c2b1cf
Hamed Bastan-Hagh <hamed@bastanhagh.com>
2021-03-08 19:40:43 +0000

    switch to static branching

--- _targets.R
+++ _targets.R
@@ -16,20 +16,25 @@ tar_option_set(
     )
 )
 
-tar_plan(
+model_values <- tibble::tibble(
+    model_type = c("tree", "elnet", "svm"), 
+    wfl_fn = rlang::syms(paste0("create_", model_type, "_wfl")), 
+    tune_fn = rlang::syms(paste0("tune_", model_type, "_grid"))
+)
+
+shared_targets <- tar_plan(
     tar_target(source_data, get_german_data("data"), format = "file"), 
     tar_target(clean_data, get_clean_data(source_data)), 
     tar_target(splits, initial_split(clean_data, prop = 0.8, strata = outcome)), 
     tar_target(folds, vfold_cv(training(splits), v = 10, strata = outcome)), 
-    tar_target(base_rec, recipe(outcome ~ ., data = training(splits))), 
-    # Decision tree
-    tar_target(tree_wfl, create_tree_wfl(base_rec)), 
-    tar_target(tree_tune, tune_tree_grid(tree_wfl, folds)), 
-    # Elastic net
-    tar_target(elnet_wfl, create_elnet_wfl(base_rec)), 
-    tar_target(elnet_tune, tune_elnet_grid(elnet_wfl, folds)), 
-    # SVM
-    tar_target(svm_wfl, create_svm_wfl(base_rec)), 
-    tar_target(svm_tune, tune_svm_grid(svm_wfl, folds))
-    
+    tar_target(base_rec, recipe(outcome ~ ., data = training(splits)))
 )
+
+model_targets <- tar_map(
+    values = model_values, 
+    names = "model_type", 
+    tar_target(wfl, wfl_fn(base_rec)), 
+    tar_target(tune, tune_fn(wfl, folds))
+)
+
+list(shared_targets, model_targets)
